55a69ccb7c356272109da5481a5f54977bad05cd
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-06-22 22:07:10 +0530

    theme toggle

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,28 @@
 import logo from "../assets/game_node.png";
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { Flex, HStack, Image, Text } from "@chakra-ui/react";
+import { ColorModeButton, useColorModeValue } from "./ui/color-mode";
 
 const NavBar = () => {
+  // This hook returns different values based on the current theme
+  // In light mode: "none", in dark mode: "invert(1)"
+  const logoFilter = useColorModeValue("none", "invert(1)");
+
   return (
-    <HStack>
-      <Image src={logo} boxSize="60px" />
-      <Text color="black">Game Node</Text>
-    </HStack>
+    <Flex justify="space-between" align="center" padding={4}>
+      <HStack>
+        <Image
+          src={logo}
+          boxSize="60px"
+          filter={logoFilter}
+          transition="filter 0.2s"
+        />
+        <Text fontSize="xl" fontWeight="bold">
+          Game Node
+        </Text>
+      </HStack>
+
+      <ColorModeButton />
+    </Flex>
   );
 };
 
