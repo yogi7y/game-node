9b80f59e11f7d0551668a8ab0d57045f14aa9420
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-06-23 11:43:08 +0530

    craete games grid and api client

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api_client";
+import { Text } from "@chakra-ui/react";
+
+interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+function GameGrid() {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((res) => {
+        setGames(res.data.results);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  }, []);
+
+  return (
+    <>
+      {error && <Text color="red">{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default GameGrid;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,10 @@ import { Flex, HStack, Image, Text } from "@chakra-ui/react";
 import { ColorModeButton, useColorModeValue } from "./ui/color-mode";
 
 const NavBar = () => {
-  // This hook returns different values based on the current theme
-  // In light mode: "none", in dark mode: "invert(1)"
   const logoFilter = useColorModeValue("none", "invert(1)");
 
   return (
-    <Flex justify="space-between" align="center" padding={4}>
+    <Flex justify="space-between" align="center" padding={10}>
       <HStack>
         <Image
           src={logo}
